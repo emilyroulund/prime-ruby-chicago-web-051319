06616daa51e198ca7479805ab2e05a3fedc05a85
Emily R <emilyroulund@gmail.com>
2019-04-18 05:29:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime? (number)
   if number <= 1
   return false
-  else  (2..number-1).to_numbers do |possible_factor|
+  else  (2..number-1).to_a.all? do |possible_factor|
       num % possible_factor != 0
    end 
  end 
