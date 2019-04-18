c8a08c34bff0b4baebb2cc2c70c31fa6d065226c
Emily R <emilyroulund@gmail.com>
2019-04-18 05:21:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 def prime? (number)
- if number % 4 != 0
+if num < 0 || num == 0
+  return false
+if num == 2 
+  return true 
+elsif (number % 2 == 0)
+   return false
+ else
    return true
+ end 
 end
